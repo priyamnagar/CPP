00b66c44d7eb6677a078cea4d9087835c9c808b0
Priyam Nagar <priyamnagar@gmail.com>
2019-05-31 18:05:33 +0530

    Add files via upload

---
+++ 0.Overview.cpp
@@ -0,0 +1,11 @@
+/* 
+-> C++ is a middle level language( It comprises a combination of both high-level and low-level language features), that          supports procedural, object-oriented, and generic programming.
+-> C++ was developed by Bjarne Stroustrup starting in 1979 at Bell Labs. It was originally named C with classes but later        renamed as C++ in 1983.
+-> C++ is a superset of C, so virtually any legal program in C can run in C++.
+-> C++ is a statically typed, compiled, general-purpose, case-sensitive, free-form programming language.
+
+-> A programming language is said to use static typing when type checking is performed during compile-time as opposed to run-    time.
+-> Generic programming is a style of computer programming in which algorithms are written in terms of types to-be-specified-    later that are then instantiated when needed for specific types provided as parameters. This style of programming is used    in Machine Learning.
+
+
+*/
