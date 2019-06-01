61d75226fa852e559f98ad891ed164e32602d961
Priyam Nagar <priyamnagar@gmail.com>
2019-06-01 23:55:15 +0530

    Add files via upload

---
+++ 3.Storage_classes.cpp
@@ -0,0 +1,10 @@
+/*
+-> A storage class defines the scope and life-time of variables. There are following
+storage classes: auto, register, static, extern, mutable.
+
+-> auto storage class is the default storage class for all local variables. It can only be used within functions i.e. local variables.
+
+-> register storage class is used to define local variables that should be stored in a register instead of RAM.
+
+
+*/
