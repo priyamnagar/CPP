a65cf8b196aecb29a6cff6a6933f1750b0085b71
Priyam Nagar <priyamnagar@gmail.com>
2019-06-11 19:47:09 +0530

    Add files via upload

---
+++ 17.Encapsulation_Abstraction.cpp
@@ -0,0 +1,39 @@
+/*
+ *
+ * Abstraction means providing only essential details and hiding background details.
+ * Data encapsulation is a mechanism of bundling the data, and the functions that use them
+ * 
+ */
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Person{
+    protected:
+        string name;
+        string address;
+        double height;
+        double weight;
+    public:
+
+        Person(string name,string address,double height,double weight){
+            this -> name = name;
+            this -> address = address;
+            this -> height = height;
+            this -> weight = weight;    
+        }
+
+        //Interface to the outside world, hiding the inner details.
+        void characterstics(){
+            std::cout << name << endl << address << endl << height <<endl << weight << endl;
+        }
+};
+
+
+int main(){
+    Person p1("Ram", "Home", 7.3, 90);
+    //Calling without knowing the inner details.
+    p1.characterstics();
+    return 0;
+}
